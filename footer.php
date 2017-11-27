b077837dc8abdf1896a5ff64a22c0985e85e48c2
Kirsten Cassidy <mantismamita@gmail.com>
2017-11-27 23:55:33 +0100

    adds grid to sidebar for medium widths

--- footer.php
+++ footer.php
@@ -22,7 +22,7 @@
 			<span class="sep"> | </span>
 			<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'gridpack' ), 'gridpack', '<a href="http://underscores.me/">Underscores.me</a>' );
+				printf( esc_html__( 'Theme: %1$s.', 'gridpack' ),'<a href="https://github.com/mantismamita/gridpack">Gridpack</a>' );
 			?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
